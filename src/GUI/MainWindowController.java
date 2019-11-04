e2147cfbc83a226ba2de0234bc56446572fa9a31
Maciej Kozłowski <kozlowm4@ee.pw.edu.pl>
2019-11-04 23:26:44 +0100

    another sketch

--- src/GUI/MainWindowController.java
+++ src/GUI/MainWindowController.java
@@ -3,6 +3,7 @@ package GUI;
 import javafx.animation.FadeTransition;
 import javafx.fxml.FXML;
 import javafx.scene.control.Button;
+import javafx.scene.control.ComboBox;
 import javafx.scene.control.TextField;
 import javafx.scene.layout.VBox;
 import javafx.stage.DirectoryChooser;
@@ -29,31 +30,47 @@ public class MainWindowController {
     private Button savePathButton;
     @FXML
     private TextField filePathTextField;
+    @FXML
+    private TextField directoryTextField;
+    @FXML
+    private ComboBox<String> algoComboBox;
 
     @FXML
     public void openDirectoryChooser() {
         DirectoryChooser directoryChooser = new DirectoryChooser();
         Stage mainStage = (Stage) savePathButton.getScene().getWindow();
         File selectedDirectory = directoryChooser.showDialog(mainStage);
+        if (selectedDirectory != null) {
+            String path = selectedDirectory.getAbsolutePath();
+            directoryTextField.setText(path);
+        }
     }
 
     @FXML
-    public void openFileChooser(){
+    public void openFileChooser() {
         FileChooser fileChooser = new FileChooser();
         Stage mainStage = (Stage) savePathButton.getScene().getWindow();
         File selectedFile = fileChooser.showOpenDialog(mainStage);
         if (selectedFile != null) {
             String path = selectedFile.getAbsolutePath();
-            System.out.println(path + "\n");
             filePathTextField.setText(path);
             animateVBox(stepTwoVBox);
         }
     }
 
-    private void animateVBox(VBox toAnimate){
-        FadeTransition ft = new FadeTransition(Duration.millis(1500), toAnimate);
-        ft.setFromValue(0.0);
-        ft.setToValue(1.0);
-        ft.play();
+    @FXML
+    public void setAlgorithm() {
+        String algoName = algoComboBox.getValue();
+        System.out.println("Algo: " + algoName);
+        animateVBox(stepThreeVBox);
+    }
+
+    private void animateVBox(VBox toAnimate) {
+        if (toAnimate.getOpacity() != 1) {
+            FadeTransition ft = new FadeTransition(Duration.millis(1500), toAnimate);
+            ft.setFromValue(0.0);
+            ft.setToValue(1.0);
+            ft.play();
+        }
     }
 }
