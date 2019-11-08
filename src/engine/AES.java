1f413a6b6406de4178422997f8270779fa80807b
DanielSporysz <daniel_spory@o2.pl>
2019-11-08 17:38:15 +0100

    added more TODOs

--- src/engine/AES.java
+++ src/engine/AES.java
@@ -1,30 +1,34 @@
 package engine;
 
-import engine.exceptions.AlgorithmException;
-
 import javax.crypto.*;
 import java.security.InvalidKeyException;
 import java.security.Key;
 import java.security.NoSuchAlgorithmException;
 
 public class AES implements Encryptor {
-    private final String algorithmName = "AES";
-    private final String algorithmMode = "none";
+    private String algorithmName;
+    private String algorithmMode;
     private Key key;
 
     private Cipher cipher;
 
     public AES(String mode, Key key){
+        //TODO null & arguments checking
+        algorithmName = "AES";
+        algorithmMode = mode;
         this.key = key;
+
         try{
-            this.cipher = Cipher.getInstance("AES/" + mode + "/PKCS5Padding");
+            this.cipher = Cipher.getInstance(algorithmName + "/" + mode + "/PKCS5Padding");
             this.cipher.init(Cipher.ENCRYPT_MODE, key);
         } catch (NoSuchAlgorithmException | NoSuchPaddingException | InvalidKeyException e){
+            //TODO suggest more suiting exception type or message
             throw new IllegalStateException("Unable to create a cipher object.");
         }
     }
 
     public byte[] encrypt(byte[] data){
+        //TODO null checking
         try{
             return cipher.doFinal(data);
         } catch (IllegalBlockSizeException | BadPaddingException e){
--- src/engine/Creator.java
+++ src/engine/Creator.java
@@ -5,8 +5,8 @@ import java.security.Key;
 
 public class Creator implements CipherCreator {
     public Encryptor createEncryptor(String algorithm, String mode, Key key) throws AlgorithmException{
-        //TODO null checking
-        //TODO more algorithms and modes
+        //TODO null & arguments checking
+        //TODO more algorithms and modes to implement
         //TODO possibly more elegant way of parsing arguments
         switch (algorithm){
             case "AES":{
@@ -14,10 +14,12 @@ public class Creator implements CipherCreator {
                     case "CBC":
                         return new AES(mode, key);
                     default:
+                        //TODO suggest more suiting exception type or message
                         throw new AlgorithmException("Not supported mode!");
                 }
             }
             default:{
+                //TODO suggest more suiting exception type or message
                 throw new AlgorithmException("Not supported algorithm!");
             }
 
