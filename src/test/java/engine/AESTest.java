8532a943c75a9fd674ac254b0b0c5171f7157579
lukitoki1 <lukitoki1@outlook.com>
2019-11-09 14:51:33 +0100

    Change directory structure
    Create an appropriate project directory structure to be able to develop
    tests for the project without creating a mess. Create test files with
    boilerplate to be developed later on.

---
+++ src/test/java/engine/AESTest.java
@@ -0,0 +1,40 @@
+package engine;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+class AESTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void encrypt() {
+    }
+
+    @Test
+    void decrypt() {
+    }
+
+    @Test
+    void getAlgorithmName() {
+    }
+
+    @Test
+    void getAlgorithmMode() {
+    }
+
+    @Test
+    void getKey() {
+    }
+
+    @Test
+    void getOperationMode() {
+    }
+}
