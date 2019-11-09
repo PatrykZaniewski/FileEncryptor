70ce1a63d6ae00f1dbd18729fcd8f50339dd490f
lukitoki1 <lukitoki1@outlook.com>
2019-11-09 22:06:37 +0100

    Create "Creator" test template and an example test.

--- src/test/engine/CreatorTest.java
+++ src/test/engine/CreatorTest.java
@@ -1,14 +1,54 @@
 package engine;
 
+import engine.exceptions.AlgorithmException;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
 
 class CreatorTest {
 
-    @Test
-    void createEncryptor() {
+    private Creator creator;
+
+    @BeforeEach
+    void setUp() {
+        creator = new Creator();
+    }
+
+    //template
+    //TODO: delete before presenting the project
+    void createEncryptor__Should() throws AlgorithmException {
+        //given
+        String algorithm = "";
+        String mode = "";
+        String key = "";
+
+        //when
+        Encryptor result = creator.createEncryptor(algorithm, mode, key);
+
+        //then
+        assertEquals(result.getAlgorithmMode(), algorithm, "");
+        assertEquals(result.getKey(), key, "");
     }
 
     @Test
-    void createDecryptor() {
+    void createEncryptor_AesCbc_ShouldReturnAesInstance() {
+        //given
+        String algorithm = "AES";
+        String mode = "CBC";
+        String key = "123";
+
+        //when
+        Encryptor result = null;
+        try {
+            result = creator.createEncryptor(algorithm, mode, key);
+        } catch (AlgorithmException e) {
+            fail("The method has thrown an exception while all the provided data is correct.");
+        }
+
+        //then
+        assertTrue(result instanceof AES, "The algorithms do not match.");
+        assertEquals(result.getAlgorithmMode(), mode, "The encryption modes do not match.");
+        assertEquals(result.getKey(), key, "The keys do not match.");
     }
 }
