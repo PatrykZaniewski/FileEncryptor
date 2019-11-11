2948861e8bb2fe58d79ccd10e4f559c7040202f7
KatarzynaWolska <kasia.wolska137@gmail.com>
2019-11-11 22:07:25 +0100

    GUI corrected.

--- src/main/GUI/MainWindowController.java
+++ src/main/GUI/MainWindowController.java
@@ -32,6 +32,8 @@ public class MainWindowController {
     @FXML
     public Label keyLabel;
     @FXML
+    public Label keyDecLabel;
+    @FXML
     private VBox stepTwoEncVBox;
     @FXML
     private VBox stepThreeEncVBox;
@@ -85,7 +87,8 @@ public class MainWindowController {
 
     @FXML
     public void openFileChooserEnc() {
-        openFileChooser(filePathTextFieldEnc, stepTwoEncVBox);
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Pliki TXT, JPEG, PNG (*.txt, *.jpeg, *.png)", "*.txt", "*.jpeg", ".png");
+        openFileChooser(filePathTextFieldEnc, stepTwoEncVBox, extFilter);
     }
 
     @FXML
@@ -95,7 +98,22 @@ public class MainWindowController {
 
     @FXML
     public void openFileChooserDec() {
-        openFileChooser(filePathTextFieldDec, stepTwoDecVBox);
+        FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Pliki JSON (*.json)",  "*.json");
+        openFileChooser(filePathTextFieldDec, stepTwoDecVBox, extFilter);
+
+        String filePath = filePathTextFieldDec.getText();
+        if (!filePath.isEmpty()) {
+            if (filePath.contains("ROT")) {
+                keyDecLabel.setText("Przesunięcie:");
+                keyDecLabel.setPrefWidth(165);
+            } else {
+                keyDecLabel.setText("Klucz:");
+                keyDecLabel.setPrefWidth(46);
+            }
+        } else {
+            keyDecLabel.setText("Klucz:");
+            keyDecLabel.setPrefWidth(46);
+        }
     }
 
     @FXML
@@ -106,7 +124,7 @@ public class MainWindowController {
     @FXML
     public void setAlgorithm() {
         animateNode(stepThreeEncVBox);
-        if (algoComboBox.getSelectionModel().getSelectedIndex() == 6) {
+        if (algoComboBox.getSelectionModel().getSelectedIndex() == 12) {
             keyLabel.setText("Przesunięcie:");
             keyLabel.setPrefWidth(165);
         } else {
@@ -153,6 +171,12 @@ public class MainWindowController {
         if (checkFiles == 1) {
             showError(1);
             return false;
+        } else if (filePath.isEmpty()) {
+            showError(7);
+            return false;
+        }  else if (savePath.isEmpty()) {
+            showError(8);
+            return false;
         } else if (checkFiles == 2) {
             showError(2);
             return false;
@@ -185,9 +209,15 @@ public class MainWindowController {
                 test = msg;
             } else {
                 algorithm = "ROT";
-                int shift = Integer.parseInt(key);
-                Encryptor encryptor = creator.createEncryptor(algorithm, shift);
-                encodedMsg = encryptor.encrypt(msg);
+
+                if(!isNumeric(key)) {
+                    showError(6);
+                    return false;
+                } else {
+                    int shift = Integer.parseInt(key);
+                    Encryptor encryptor = creator.createEncryptor(algorithm, shift);
+                    encodedMsg = encryptor.encrypt(msg);
+                }
             }
             obj.put("Algorithm", algorithm);
             obj.put("Encrypted", Base64.getEncoder().encodeToString(encodedMsg));
@@ -220,6 +250,12 @@ public class MainWindowController {
         } else if (keyDec.equals("")) {
             showError(3);
             return false;
+        } else if (filePath.isEmpty()) {
+            showError(7);
+            return false;
+        } else if (savePath.isEmpty()) {
+            showError(8);
+            return false;
         }
 
         FileReader file = new FileReader(filePath);
@@ -233,7 +269,12 @@ public class MainWindowController {
         byte[] decoded_msg = null;
 
         if (algorithm.equals("ROT")) {
-            decryptor = creator.createDecryptor("ROT", Integer.parseInt(keyDec));
+            if (!isNumeric(keyDec)) {
+                showError(6);
+                return false;
+            } else {
+                decryptor = creator.createDecryptor("ROT", Integer.parseInt(keyDec));
+            }
         } else {
             String mode = (String) jsonObject.get("Mode");
             String iv = (String) jsonObject.get("Iv");
@@ -296,6 +337,15 @@ public class MainWindowController {
             case 5:
                 mes = "Nieprawidłowa długość klucza. Dla trybu AES wynosi ona 8 znaków.";
                 break;
+            case 6:
+                mes = "Przesunięcie dla trybu ROT musi być liczbą.";
+                break;
+            case 7:
+                mes = "Nie podano pliku.";
+                break;
+            case 8:
+                mes = "Nie podano folderu docelowego.";
+                break;
             default:
                 mes = "Wystąpił bład";
                 break;
@@ -329,16 +379,24 @@ public class MainWindowController {
         }
     }
 
-    private void openFileChooser(TextField pathField, VBox toAnimate) {
+    private void openFileChooser(TextField pathField, VBox toAnimate, FileChooser.ExtensionFilter extFilter) {
         FileChooser fileChooser = new FileChooser();
-        //FileChooser.ExtensionFilter extFilter = new FileChooser.ExtensionFilter("Pliki TXT (*.txt)", "*.txt", "*.json");
-        //fileChooser.getExtensionFilters().add(extFilter);
+        fileChooser.getExtensionFilters().add(extFilter);
         Stage mainStage = (Stage) savePathButtonEnc.getScene().getWindow();
         File selectedFile = fileChooser.showOpenDialog(mainStage);
         if (selectedFile != null) {
             String path = selectedFile.getAbsolutePath();
             pathField.setText(path);
             animateNode(toAnimate);
+        }
+    }
+
+    private boolean isNumeric(String str) {
+        try {
+            Integer.parseInt(str);
+            return true;
+        } catch (NumberFormatException e) {
+            return false;
         }
     }
 }
