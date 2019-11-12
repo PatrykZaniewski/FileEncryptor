f7a728afc65252c564a746c386b07680f2842b74
lukitoki1 <lukitoki1@outlook.com>
2019-11-12 09:08:28 +0100

    Transform tests to work with new class definitions.
    Update tests to be able to cooperate with the new interfaces in the
    code.

--- src/test/engine/AESTest.java
+++ src/test/engine/AESTest.java
@@ -1,5 +1,6 @@
 package engine;
 
+import engine.exceptions.AlgorithmException;
 import org.junit.jupiter.api.Test;
 
 import javax.crypto.Cipher;
@@ -9,10 +10,10 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class AESTest {
 
-    private AES createAesInstance_ValidData_HelperMethod(String mode, String key, int operationMode) {
+    private AES createAesInstance_ValidData_HelperMethod(String mode, String key, byte[] iv, int operationMode) {
         AES aes = null;
         try {
-            aes = new AES(mode, key, operationMode);
+            aes = new AES(mode, key, iv, operationMode);
         } catch (Exception e) {
             fail("Exception thrown even though all arguments are correct");
         }
@@ -24,15 +25,17 @@ class AESTest {
         //given
         String mode = "CBC";
         String key = "123";
+        byte[] iv = new byte[16];
         int operationMode = Cipher.ENCRYPT_MODE;
 
         //when
-        AES aes = createAesInstance_ValidData_HelperMethod(mode, key, operationMode);
+        AES aes = createAesInstance_ValidData_HelperMethod(mode, key, iv, operationMode);
 
         //then
         assertEquals(aes.getAlgorithmName(), "AES", "The algorithmName value is incorrect.");
         assertEquals(aes.getAlgorithmMode(), mode, "The algorithmMode value is incorrect.");
         assertEquals(aes.getKey(), key, "The key value is incorrect");
+        assertNotNull(aes.getIv(), "The IV parameter is null");
         assertEquals(aes.getOperationMode(), operationMode, "The operationMode value is incorrect.");
     }
 
@@ -41,13 +44,14 @@ class AESTest {
         //given
         String mode = null;
         String key = "123";
+        byte[] iv = new byte[16];
         int operationMode = Cipher.ENCRYPT_MODE;
 
         //when
         AES aes = null;
 
         //then
-        assertThrows(IllegalArgumentException.class, () -> new AES(mode, key, operationMode), "The class didn't throw an IllegalArgumentException indicating that it recognized a mode is null.");
+        assertThrows(IllegalArgumentException.class, () -> new AES(mode, key, iv, operationMode), "The class didn't throw an IllegalArgumentException indicating that it recognized a mode is null.");
     }
 
     @Test
@@ -55,13 +59,14 @@ class AESTest {
         //given
         String mode = "CBC";
         String key = null;
+        byte[] iv = new byte[16];
         int operationMode = Cipher.ENCRYPT_MODE;
 
         //when
         AES aes = null;
 
         //then
-        assertThrows(IllegalArgumentException.class, () -> new AES(mode, key, operationMode), "The class didn't throw an IllegalArgumentException indicating that it recognized a key is null.");
+        assertThrows(IllegalArgumentException.class, () -> new AES(mode, key, iv, operationMode), "The class didn't throw an IllegalArgumentException indicating that it recognized a key is null.");
     }
 
     @Test
@@ -69,13 +74,14 @@ class AESTest {
         //given
         String mode = "AES";
         String key = "123";
+        byte[] iv = new byte[16];
         int operationMode = -123;
 
         //when
         AES aes = null;
 
         //then
-        assertThrows(IllegalArgumentException.class, () -> new AES(mode, key, operationMode), "The class didn't throw an IllegalArgumentException indicating that it recognized an operationMode is null.");
+        assertThrows(AlgorithmException.class, () -> new AES(mode, key, iv, operationMode), "The class didn't throw an AlgorithmException indicating that it recognized an operationMode is null.");
     }
 
     @Test
@@ -83,12 +89,13 @@ class AESTest {
         //given
         String mode = "CBC";
         String key = "123";
+        byte[] iv = new byte[16];
         int operationMode = Cipher.ENCRYPT_MODE;
 
         byte[] plainData = "alamakota".getBytes(StandardCharsets.UTF_8);
 
         //when
-        AES aes = createAesInstance_ValidData_HelperMethod(mode, key, operationMode);
+        AES aes = createAesInstance_ValidData_HelperMethod(mode, key, iv, operationMode);
         byte[] encryptedData = null;
         try {
             encryptedData = aes.encrypt(plainData);
--- src/test/engine/CreatorTest.java
+++ src/test/engine/CreatorTest.java
@@ -5,10 +5,7 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 
 import javax.crypto.Cipher;
-import java.security.NoSuchAlgorithmException;
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.nio.charset.StandardCharsets;
 
 import static org.junit.jupiter.api.Assertions.*;
 
@@ -21,218 +18,306 @@ class CreatorTest {
         creator = new Creator();
     }
 
+    //ENCRYPTOR
+
     @Test
     void createEncryptor_InvalidAlgorithm_ShouldThrowException() {
         //given
         String algorithm = "Lorem ipsum";
+
+        int shift = 1;
+
         String mode = "CBC";
         String key = "123";
+        byte[] iv = "iviviv".getBytes(StandardCharsets.UTF_8);
 
         //when & then
-        assertThrows(NoSuchAlgorithmException.class, () -> creator.createEncryptor(algorithm, mode, key), "Didn't throw NoSuchAlgorithmException");
+        String errorMessage = "Method createEncryptor() didn't throw AlgorithmException while algorithm is unknown";
+
+        assertThrows(AlgorithmException.class, () -> creator.createEncryptor(algorithm, shift), errorMessage);
+        assertThrows(AlgorithmException.class, () -> creator.createEncryptor(algorithm, mode, key, iv), errorMessage);
     }
 
     @Test
-    void createEncryptor_InvalidAlgorithmMode_ShouldThrowException() {
+    void createEncryptor_NullAlgorithm_ShouldThrowException() {
         //given
-        List<String> algorithms = new ArrayList<>(Arrays.asList("DES", "AES"));
-        String mode = "Lorem ipsum";
+        String algorithm = null;
+
+        int shift = 1;
+
+        String mode = "CBC";
         String key = "123";
+        byte[] iv = "iviviv".getBytes(StandardCharsets.UTF_8);
 
         //when & then
-        for (String algorithm : algorithms) {
-            assertThrows(AlgorithmException.class, () -> creator.createEncryptor(algorithm, mode, key), "Algorithm " + algorithm + "didn't throw AlgorithmException");
-        }
+        String errorMessage = "Method createEncryptor() didn't throw IllegalArgumentException while algorithm is null";
+        assertThrows(IllegalArgumentException.class, () -> creator.createEncryptor(algorithm, shift), errorMessage);
+        assertThrows(IllegalArgumentException.class, () -> creator.createEncryptor(algorithm, mode, key, iv), errorMessage);
     }
 
-    private Encryptor createEncryptor_ValidData_HelperMethod(String algoName, String mode, String key, int operationMode) {
+    @Test
+    void createEncryptor_NullData_ShouldThrowException() {
+        //given
+        String algorithm = "AES";
+
+        String mode = "CBC";
+        String key = "123";
+        byte[] iv = "iviviv".getBytes(StandardCharsets.UTF_8);
+
+        //when & then
+        String errorMessage = "Method createEncryptor(mode) didn't throw IllegalArgumentException while mode is null";
+
+        assertThrows(IllegalArgumentException.class, () -> creator.createEncryptor(algorithm, null, key, iv), errorMessage);
+        assertThrows(IllegalArgumentException.class, () -> creator.createEncryptor(algorithm, mode, null, iv), errorMessage);
+    }
+
+    private Encryptor createEncryptor_ValidData_HelperMethod(String algoName, byte[] iv) {
+        String mode = "CBC";
+        String key = "123";
+        int operationMode = Cipher.ENCRYPT_MODE;
+
         //when
         Encryptor result = null;
         try {
-            result = creator.createEncryptor(algoName, mode, key);
-        } catch (AlgorithmException | NoSuchAlgorithmException e) {
+            result = creator.createEncryptor(algoName, mode, key, iv);
+        } catch (AlgorithmException e) {
+            e.printStackTrace();
             fail("The method has thrown an exception while all the provided data is correct.");
         }
 
         //then
-        assertEquals(result.getOperationMode(), operationMode, "The operation mode is incorrect.");
+        assertEquals(result.getAlgorithmName(), algoName, "The algorithm name is incorrect.");
         assertEquals(result.getAlgorithmMode(), mode, "The encryption modes do not match.");
         assertEquals(result.getKey(), key, "The keys do not match.");
+        assertNotNull(result.getIv(), "IV has not been generated.");
+        assertEquals(result.getOperationMode(), operationMode, "The operation mode is incorrect.");
+
+        return result;
+    }
+
+    private Encryptor createEncryptor_ValidData_HelperMethod(String algoName, int shift) {
+        //when
+        Encryptor result = null;
+        try {
+            result = creator.createEncryptor(algoName, shift);
+        } catch (AlgorithmException e) {
+            fail("The method has thrown an exception while all the provided data is correct.");
+        }
+
+        //then
+        assertEquals(result.getAlgorithmName(), "ROT", "The algorithm name is incorrect.");
+        assertEquals(result.getAlgorithmMode(), "ROT cipher object does not have modes.", "The encryption modes do not match.");
+        assertEquals(result.getKey(), "" + shift, "The keys do not match.");
+        assertNull(result.getIv(), "IV value is not null even though the algorithm doesn't use this variable.");
+        assertEquals(result.getOperationMode(), Cipher.ENCRYPT_MODE, "The operation mode is incorrect.");
 
         return result;
     }
 
     @Test
-    void createEncryptor_AesCbc_ShouldReturnAesInstance() {
+    void createEncryptor_Rot_ShouldReturnRotInstance() {
+        //given
+        String algorithm = "ROT";
+        int shift = 1;
+
+        //when & then
+        Encryptor result = createEncryptor_ValidData_HelperMethod(algorithm, shift);
+        assertTrue(result instanceof ROT, "The algorithm is not an instance of " + algorithm + ".");
+    }
+
+    @Test
+    void createEncryptor_Aes_ShouldReturnAesInstance() {
         //given
         String algorithm = "AES";
+        byte[] iv = null;
+
+        //when & then
+        Encryptor result = createEncryptor_ValidData_HelperMethod(algorithm, iv);
+        assertTrue(result instanceof AES, "The algorithm is not an instance of " + algorithm + ".");
+    }
+
+    @Test
+    void createEncryptor_Des_ShouldReturnDesInstance() {
+        //given
+        String algorithm = "DES";
+        byte[] iv = null;
+
+        //when & then
+        Encryptor result = createEncryptor_ValidData_HelperMethod(algorithm, iv);
+        assertTrue(result instanceof DES, "The algorithm is not an instance of " + algorithm + ".");
+    }
+
+    @Test
+    void createEncryptor_Rc2_ShouldReturnRc2Instance() {
+        //given
+        String algorithm = "RC2";
+        byte[] iv = null;
+
+        //when & then
+        Encryptor result = createEncryptor_ValidData_HelperMethod(algorithm, iv);
+        assertTrue(result instanceof RC2, "The algorithm is not an instance of " + algorithm + ".");
+    }
+
+    @Test
+    void createEncryptor_Blowfish_ShouldReturnBlowfishInstance() {
+        //given
+        String algorithm = "Blowfish";
+        byte[] iv = null;
+
+        //when & then
+        Encryptor result = createEncryptor_ValidData_HelperMethod(algorithm, iv);
+        assertTrue(result instanceof Blowfish, "The algorithm is not an instance of " + algorithm + ".");
+    }
+
+    // DECRYPTOR
+
+    @Test
+    void createDecryptor_InvalidAlgorithm_ShouldThrowException() {
+        //given
+        String algorithm = "Lorem ipsum";
+
+        int shift = 1;
+
         String mode = "CBC";
         String key = "123";
-        int operationMode = Cipher.ENCRYPT_MODE;
+        byte[] iv = "iviviv".getBytes(StandardCharsets.UTF_8);
 
         //when & then
-        Encryptor result = createEncryptor_ValidData_HelperMethod(algorithm, mode, key, operationMode);
-        assertTrue(result instanceof AES, "The algorithm is not an instance of " + algorithm + ".");
+        String errorMessage = "Method createDecryptor() didn't throw AlgorithmException while algorithm is unknown";
+
+        assertThrows(AlgorithmException.class, () -> creator.createDecryptor(algorithm, shift), errorMessage);
+        assertThrows(AlgorithmException.class, () -> creator.createDecryptor(algorithm, mode, key, iv), errorMessage);
     }
 
     @Test
-    void createEncryptor_AesEcb_ShouldReturnAesInstance() {
+    void createDecryptor_NullAlgorithm_ShouldThrowException() {
         //given
-        String algorithm = "AES";
-        String mode = "ECB";
-        String key = "123";
-        int operationMode = Cipher.ENCRYPT_MODE;
+        String algorithm = null;
 
-        //when & then
-        Encryptor result = createEncryptor_ValidData_HelperMethod(algorithm, mode, key, operationMode);
-        assertTrue(result instanceof AES, "The algorithm is not an instance of " + algorithm + ".");
-    }
+        int shift = 1;
 
-    @Test
-    void createEncryptor_AesCfb_ShouldReturnAesInstance() {
-        //given
-        String algorithm = "AES";
-        String mode = "CFB";
-        String key = "123";
-        int operationMode = Cipher.ENCRYPT_MODE;
-
-        //when & then
-        Encryptor result = createEncryptor_ValidData_HelperMethod(algorithm, mode, key, operationMode);
-        assertTrue(result instanceof AES, "The algorithm is not an instance of " + algorithm + ".");
-    }
-
-    @Test
-    void createEncryptor_DesCbc_ShouldReturnDesInstance() {
-        //given
-        String algorithm = "DES";
         String mode = "CBC";
-        //String key = new String(new char[16]).replace('\0', 'a');
         String key = "123";
-        int operationMode = Cipher.ENCRYPT_MODE;
+        byte[] iv = "iviviv".getBytes(StandardCharsets.UTF_8);
 
         //when & then
-        Encryptor result = createEncryptor_ValidData_HelperMethod(algorithm, mode, key, operationMode);
-        assertTrue(result instanceof DES, "The algorithm is not an instance of " + algorithm + ".");
+        String errorMessage = "Method createDecryptor() didn't throw IllegalArgumentException while algorithm is null";
+        assertThrows(IllegalArgumentException.class, () -> creator.createDecryptor(algorithm, shift), errorMessage);
+        assertThrows(IllegalArgumentException.class, () -> creator.createDecryptor(algorithm, mode, key, iv), errorMessage);
     }
 
     @Test
-    void createEncryptor_DesEcb_ShouldReturnDesInstance() {
+    void createDecryptor_NullData_ShouldThrowException() {
         //given
-        String algorithm = "DES";
-        String mode = "ECB";
+        String algorithm = "AES";
+
+        String mode = "CBC";
         String key = "123";
-        int operationMode = Cipher.ENCRYPT_MODE;
+        byte[] iv = "iviviv".getBytes(StandardCharsets.UTF_8);
 
         //when & then
-        Encryptor result = createEncryptor_ValidData_HelperMethod(algorithm, mode, key, operationMode);
-        assertTrue(result instanceof DES, "The algorithm is not an instance of " + algorithm + ".");
+        String errorMessage = "Method createDecryptor(mode) didn't throw IllegalArgumentException while mode is null";
+
+        assertThrows(IllegalArgumentException.class, () -> creator.createDecryptor(algorithm, null, key, iv), errorMessage);
+        assertThrows(IllegalArgumentException.class, () -> creator.createDecryptor(algorithm, mode, null, iv), errorMessage);
     }
 
-    @Test
-    void createEncryptor_DesCfb_ShouldReturnDesInstance() {
-        //given
-        String algorithm = "DES";
-        String mode = "CFB";
+    private Decryptor createDecryptor_ValidData_HelperMethod(String algoName, byte[] iv) {
+        //when
+        String mode = "CBC";
         String key = "123";
-        int operationMode = Cipher.ENCRYPT_MODE;
+        int operationMode = Cipher.DECRYPT_MODE;
 
-        //when & then
-        Encryptor result = createEncryptor_ValidData_HelperMethod(algorithm, mode, key, operationMode);
-        assertTrue(result instanceof DES, "The algorithm is not an instance of " + algorithm + ".");
+        Decryptor result = null;
+        try {
+            result = creator.createDecryptor(algoName, mode, key, iv);
+        } catch (AlgorithmException e) {
+            e.printStackTrace();
+            fail("The method has thrown an exception while all the provided data is correct.");
+        }
+
+        //then
+        assertEquals(result.getAlgorithmName(), algoName, "The algorithm name is incorrect.");
+        assertEquals(result.getAlgorithmMode(), mode, "The encryption modes do not match.");
+        assertEquals(result.getKey(), key, "The keys do not match.");
+        assertNotNull(result.getIv(), "The IV parameter is null");
+        assertEquals(result.getOperationMode(), operationMode, "The operation mode is incorrect.");
+
+        return result;
     }
 
-    private Decryptor createDecryptor_ValidData_HelperMethod(String algoName, String mode, String key, int operationMode) {
+    private Decryptor createDecryptor_ValidData_HelperMethod(String algoName, int shift) {
         //when
         Decryptor result = null;
         try {
-            result = creator.createDecryptor(algoName, mode, key);
-        } catch (AlgorithmException | NoSuchAlgorithmException e) {
+            result = creator.createDecryptor(algoName, shift);
+        } catch (AlgorithmException e) {
+            e.printStackTrace();
             fail("The method has thrown an exception while all the provided data is correct.");
         }
 
         //then
-        assertEquals(result.getOperationMode(), operationMode, "The operation mode is incorrect.");
-        assertEquals(result.getAlgorithmMode(), mode, "The encryption modes do not match.");
-        assertEquals(result.getKey(), key, "The keys do not match.");
+        assertEquals(result.getAlgorithmName(), "ROT", "The algorithm name is incorrect.");
+        assertEquals(result.getAlgorithmMode(), "ROT cipher object does not have modes.", "The encryption modes do not match.");
+        assertEquals(result.getKey(), "" + shift, "The keys do not match.");
+        assertNull(result.getIv(), "IV value is not null even though the algorithm doesn't use this variable.");
+        assertEquals(result.getOperationMode(), Cipher.DECRYPT_MODE, "The operation mode is incorrect.");
 
         return result;
     }
 
     @Test
-    void createDecryptor_AesCbc_ShouldReturnAesInstance() {
+    void createDecryptor_Rot_ShouldReturnRotInstance() {
         //given
-        String algorithm = "AES";
-        String mode = "CBC";
-        String key = "123";
-        int operationMode = Cipher.DECRYPT_MODE;
+        String algorithm = "ROT";
+        int shift = 1;
 
         //when & then
-        Decryptor result = createDecryptor_ValidData_HelperMethod(algorithm, mode, key, operationMode);
+        Decryptor result = createDecryptor_ValidData_HelperMethod(algorithm, shift);
+        assertTrue(result instanceof ROT, "The algorithm is not an instance of " + algorithm + ".");
+    }
+
+    @Test
+    void createDecryptor_Aes_ShouldReturnAesInstance() {
+        //given
+        String algorithm = "AES";
+        byte[] iv = new byte[16];
+
+        //when & then
+        Decryptor result = createDecryptor_ValidData_HelperMethod(algorithm, iv);
         assertTrue(result instanceof AES, "The algorithm is not an instance of " + algorithm + ".");
     }
 
     @Test
-    void createDecryptor_AesEcb_ShouldReturnAesInstance() {
-        //given
-        String algorithm = "AES";
-        String mode = "ECB";
-        String key = "123";
-        int operationMode = Cipher.DECRYPT_MODE;
-
-        //when & then
-        Decryptor result = createDecryptor_ValidData_HelperMethod(algorithm, mode, key, operationMode);
-        assertTrue(result instanceof AES, "The algorithm is not an instance of " + algorithm + ".");
-    }
-
-    @Test
-    void createDecryptor_AesCfb_ShouldReturnAesInstance() {
-        //given
-        String algorithm = "AES";
-        String mode = "CFB";
-        String key = "123";
-        int operationMode = Cipher.DECRYPT_MODE;
-
-        //when & then
-        Decryptor result = createDecryptor_ValidData_HelperMethod(algorithm, mode, key, operationMode);
-        assertTrue(result instanceof AES, "The algorithm is not an instance of " + algorithm + ".");
-    }
-
-    @Test
-    void createDecryptor_DesCbc_ShouldReturnDesInstance() {
+    void createDecryptor_Des_ShouldReturnDesInstance() {
         //given
         String algorithm = "DES";
-        String mode = "CBC";
-        String key = "123";
-        int operationMode = Cipher.DECRYPT_MODE;
+        byte[] iv = new byte[8];
 
         //when & then
-        Decryptor result = createDecryptor_ValidData_HelperMethod(algorithm, mode, key, operationMode);
+        Decryptor result = createDecryptor_ValidData_HelperMethod(algorithm, iv);
         assertTrue(result instanceof DES, "The algorithm is not an instance of " + algorithm + ".");
     }
 
     @Test
-    void createDecryptor_DesEcb_ShouldReturnDesInstance() {
+    void createDecryptor_Rc2_ShouldReturnRc2Instance() {
         //given
-        String algorithm = "DES";
-        String mode = "ECB";
-        String key = "123";
-        int operationMode = Cipher.DECRYPT_MODE;
+        String algorithm = "RC2";
+        byte[] iv = new byte[8];
 
         //when & then
-        Decryptor result = createDecryptor_ValidData_HelperMethod(algorithm, mode, key, operationMode);
-        assertTrue(result instanceof DES, "The algorithm is not an instance of " + algorithm + ".");
+        Decryptor result = createDecryptor_ValidData_HelperMethod(algorithm, iv);
+        assertTrue(result instanceof RC2, "The algorithm is not an instance of " + algorithm + ".");
     }
 
     @Test
-    void createDecryptor_DesCfb_ShouldReturnDesInstance() {
+    void createDecryptor_Blowfish_ShouldReturnBlowfishInstance() {
         //given
-        String algorithm = "DES";
-        String mode = "CFB";
-        String key = "123";
-        int operationMode = Cipher.DECRYPT_MODE;
+        String algorithm = "Blowfish";
+        byte[] iv = new byte[8];
 
         //when & then
-        Decryptor result = createDecryptor_ValidData_HelperMethod(algorithm, mode, key, operationMode);
-        assertTrue(result instanceof DES, "The algorithm is not an instance of " + algorithm + ".");
+        Decryptor result = createDecryptor_ValidData_HelperMethod(algorithm, iv);
+        assertTrue(result instanceof Blowfish, "The algorithm is not an instance of " + algorithm + ".");
     }
 }
