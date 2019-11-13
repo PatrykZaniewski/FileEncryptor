c0945f142e0c1a728a721378f508916ee3f5c726
DanielSporysz <daniel_spory@o2.pl>
2019-11-13 10:11:00 +0100

    deleted unused exception classes

--- src/main/engine/exceptions/FileExtensionException.java
+++
@@ -1,9 +0,0 @@
-package engine.exceptions;
-/*
-Accept only *.rsa files
- */
-public class FileExtensionException extends Exception {
-    public FileExtensionException(String message){
-        super(message);
-    }
-}
--- src/main/engine/exceptions/FileSizeException.java
+++
@@ -1,7 +0,0 @@
-package engine.exceptions;
-
-public class FileSizeException extends Exception{
-    public FileSizeException(String message){
-        super(message);
-    }
-}
