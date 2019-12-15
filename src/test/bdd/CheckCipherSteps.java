463a085522191a96f73626081b2910fc8947129f
Unknown <kajgore@vp.pl>
2019-12-15 14:25:15 +0100

    Cipher bdd added

---
+++ src/test/bdd/CheckCipherSteps.java
@@ -0,0 +1,54 @@
+package bdd;
+
+import engine.*;
+import engine.exceptions.AlgorithmException;
+import org.jbehave.core.annotations.Given;
+import org.jbehave.core.annotations.Then;
+import org.jbehave.core.annotations.When;
+
+import javax.crypto.Cipher;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Paths;
+import java.util.Base64;
+
+import static org.junit.Assert.assertEquals;
+
+public class CheckCipherSteps {
+    private byte[] file;
+    private Encryptor encryptor;
+
+    @Given("a $path file")
+    public void getTheFile(String path) {
+        String pwd = System.getProperty("user.dir");
+        try {
+            file = Files.readAllBytes(Paths.get(pwd + path));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @When("I choose the $algoType algorithm with $mode mode and $password password and $iv iv")
+    public void chooseAlgo(String algoType, String mode, String password, String iv) throws AlgorithmException {
+        switch (algoType) {
+            case "AES":
+                encryptor = new AES(mode, password, Base64.getDecoder().decode(iv), Cipher.ENCRYPT_MODE);
+                break;
+            case "DES":
+                encryptor = new DES(mode, password, Base64.getDecoder().decode(iv), Cipher.ENCRYPT_MODE);
+                break;
+            case "Blowfish":
+                encryptor = new Blowfish(mode, password, Base64.getDecoder().decode(iv), Cipher.ENCRYPT_MODE);
+                break;
+            case "RC2":
+                encryptor = new RC2(mode, password, Base64.getDecoder().decode(iv), Cipher.ENCRYPT_MODE);
+                break;
+        }
+    }
+
+    @Then("I get the proper cipheredText ($ciphered)")
+    public void getCipher(String ciphered) {
+        byte[] ans = encryptor.encrypt(file);
+        assertEquals(ciphered, Base64.getEncoder().encodeToString(ans));
+    }
+}
---
+++ src/test/bdd/i_get_the_cipher.story
@@ -0,0 +1,9 @@
+GIVEN a /src/test/bdd/loremCipher.txt file
+WHEN I choose the AES algorithm with CBC mode and abc password and Uw7EzaO5V2fae5OcU8BNyg== iv
+THEN I get the proper cipheredText (nFSKRcSC\/DPI5Q8MTMX0g\/pKo2GZ9TYJMeFVqHtCOTQ=",")
+
+
+
+
+
+
